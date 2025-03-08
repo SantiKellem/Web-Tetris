2936b37e7ef8131c5071d2692134b448bf34a79a
SantiKellem <santikellemberger@gmail.com>
2025-03-08 20:26:19 -0300

    Adding gravity and piece rotation, need to fix bugs collisions

--- index.js
+++ index.js
@@ -53,56 +53,69 @@ const startButton = document.getElementById("startButton");
 
 // Create tetris pieces
 const SQUARE = {
-    position: {x: 7, y: 0},
+    position: {x: 0, y: 0},
     shape: [[1, 1],
             [1, 1]]}
 
 const LINE = {
-    position: {x: 6, y: 0},
+    position: {x: 0, y: 0},
     shape: [[1],
             [1],
             [1],
             [1]]}
 
 const EL_LEFT = {
-    position: {x: 6, y: 0},
+    position: {x: 0, y: 0},
     shape: [[0, 1],
             [0, 1],
             [1, 1]]}
 
 const EL_RIGHT = {
-    position: {x: 6, y: 0},
+    position: {x: 0, y: 0},
     shape: [[1, 0],
             [1, 0],
             [1, 1]]}
 
 const STEP = {
-    position: {x: 6, y: 0},
+    position: {x: 0, y: 0},
     shape: [[1, 1, 1],
             [0, 1, 0]]}
 
 const Z_RIGHT = {
-    position: {x: 6, y: 0},
+    position: {x: 0, y: 0},
     shape: [[0, 1, 1],
             [1, 1, 0]]}
 
 const Z_LEFT = {
-    position: {x: 6, y: 0},
+    position: {x: 0, y: 0},
     shape: [[1, 1, 0],
             [0, 1, 1]]}
 
 const PIECES = [SQUARE, LINE, EL_LEFT, EL_RIGHT, STEP, Z_RIGHT, Z_LEFT];
 
 
+let intervalDrops = 1000;
+let lastDrop = 0;
+let level = 0;
+let lastIntervalChangeLevel = 0;
 
 // Game Loop
-const FPS = 60;
-const FRAME_TIME = 1000 / FPS;
-
 function update() {
-    setTimeout(update, FRAME_TIME);
-    draw();
+    if (intervalDrops > 500 && level > lastIntervalChangeLevel + 9) {
+            intervalDrops -= 100;
+            lastIntervalChangeLevel = level;
+            console.log(intervalDrops);
+            console.log(level);
+    }
 
+    let currentTime = performance.now();
+    if (currentTime - lastDrop > intervalDrops) {
+        lastDrop = currentTime;
+        piece.position.y++;
+        collisionDetect();
+    }
+    draw();
+    requestAnimationFrame(update);
 }
 
 // Draw function
@@ -110,8 +123,8 @@ function draw() {
     canvasContext.fillStyle = "#000";
     canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
-    board.forEach((arr, y) => 
-        arr.forEach((el, x) => {
+    board.forEach((row, y) => 
+        row.forEach((el, x) => {
             if (el === 1) {
                 canvasContext.fillStyle = "yellow";
                 canvasContext.fillRect(x, y, 1, 1);
@@ -131,28 +144,53 @@ function draw() {
 
 // Piece movement
 document.addEventListener("keydown", event => {
-    if (event.key === "ArrowRight") {
-        event.preventDefault();
-        piece.position.x++;
+    event.preventDefault();
+    switch (event.key) {
+        case "ArrowRight":
+            piece.position.x++;
+            break;
+        case "ArrowLeft":
+            piece.position.x--;
+            break;
+        case "ArrowDown":
+            piece.position.y++;
+            break;
+        case "ArrowUp":
+            rotatePiece();
+            break;
+        default:
+            break;
     }
-    if (event.key === "ArrowLeft") {
-        event.preventDefault();
-        piece.position.x--;
-    }
-    if (event.key === "ArrowDown") {
-        event.preventDefault();
-        piece.position.y++;
-    }
-    if (event.key === "ArrowUp") {
-        event.preventDefault();
-        piece.position.y--;
-    }
-    colisionDetect();
+    collisionDetect();
     
 });
 
+// Piece Rotation
+function rotatePiece() {
+    let newShape = [];
+
+    // number of columns of newShape = number of rows of piece.shape
+    for (let x = 0; x <= piece.shape[0].length - 1; x++) {
+        newShape[x] = [];
+    }
+
+    // Transpose matrix
+    for (let y = 0; y <= piece.shape.length - 1; y++) {
+        for (let x = 0; x <= piece.shape[y].length - 1; x++) {
+            newShape[x][y] = piece.shape[y][x];
+        }
+    }
+    
+    // Reverse each row 
+    for (let i = 0; i < newShape.length; i++) {
+        newShape[i].reverse();
+    }
+
+    piece.shape = newShape;
+}
+
 // Collision Detector
-function colisionDetect() {
+function collisionDetect() {
     if (piece.position.x > (canvas.width / BLOCK_SIZE) - piece.shape[0].length) piece.position.x--;
     if (piece.position.x < 0) piece.position.x++;
     if (piece.position.y > (canvas.height / BLOCK_SIZE) - piece.shape.length) {
@@ -172,6 +210,8 @@ function colisionDetect() {
         piece.position.y--;
         fixPiece();
     }
+
+    // IDEA --> for each x position until finishing the piece 
 
     /*if (board[piece.position.y + piece.shape.length - 1][piece.position.x] === 1) {
         piece.position.y--;
@@ -193,6 +233,7 @@ function fixPiece() {
         startButton.style.display = "block";
     }
     else {
+        level++;
         piece = createNewPiece();
     }
 }
@@ -243,6 +284,6 @@ function startGame() {
     score = 0;
     scoreText.textContent = `Score: ${score}`;
     piece = createNewPiece();
-    update();
+    requestAnimationFrame(update);
 }
 
